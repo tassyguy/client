dced15ba8571a22713b97c5913b0f69c8a34d2d5
Simon Phillips <simonxphillips@gmail.com>
2020-08-29 15:53:45 -0400

    Initial layout commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout'
 
 class App extends Component {
   state = {
@@ -27,11 +28,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header> 
         <p className="App-intro">{this.state.data}</p>
+        <Layout></Layout>
       </div>
     );
   }
